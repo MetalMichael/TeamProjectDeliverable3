24718919f6017f6c0fc146837f7ec6c095bbfc2e
MetalMichael <michaelfiford@gmail.com>
2014-03-10 22:44:58 +0000

    Initial Commit 2

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Team_Project_Deliverable_3.MvcApplication" Language="C#" %>
---
+++ Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team_Project_Deliverable_3.Models
+{
+    public class Department
+    {
+        private string code { get; set; }
+        public string departmentCode { get; set; }
+        public string departmentName { get; set; }
+    }
+}
