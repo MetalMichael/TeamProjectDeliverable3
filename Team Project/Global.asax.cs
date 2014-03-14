d0a61cbf3b556139bd00bcfe7dc3b7d5718a1ffe
MetalMichael <michaelfiford@gmail.com>
2014-03-14 14:07:48 +0000

    Some changes

--- Team Project/Global.asax.cs
+++ Team Project/Global.asax.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.IO;
+using System.Reflection;
+
 
 namespace TimetableSystem
 {
@@ -27,8 +30,56 @@ namespace TimetableSystem
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            ViewEngines.Engines.Add(new RazorViewEngine());
             RegisterRoutes(RouteTable.Routes);
+            ViewEngines.Engines.Add(new RazorViewEngine());
         }
     }
 }
+    public class RazorViewEngine : VirtualPathProviderViewEngine
+    {
+        public RazorViewEngine()
+        {
+            MasterLocationFormats = new[] { "~/Views/{1}/{0}.cshtml" };
+            AreaMasterLocationFormats = new[] { "~/Areas/{2}/Views/{1}/{0}.cshtml" };
+            PartialViewLocationFormats = ViewLocationFormats = new[] {  
+                "~/Views/{1}/{0}.cshtml", "~/Views/Shared/{0}.cshtml"};
+            AreaPartialViewLocationFormats = AreaViewLocationFormats = new[] {  
+                "~/Areas/{2}/Views/{1}/{0}.cshtml",  
+                "~/Areas/{2}/Views/Shared/{0}.cshtml"};
+        }
+
+        protected override IView CreatePartialView(
+            ControllerContext controllerContext,
+            string partialPath)
+        {
+            return new RazorView { ViewPath = partialPath };
+        }
+
+        protected override IView CreateView(
+            ControllerContext controllerContext,
+            string viewPath, string masterPath)
+        {
+            return new RazorView { ViewPath = viewPath };
+        }
+
+        private class RazorView : IView
+        {
+            public string ViewPath;
+
+            public void Render(ViewContext viewContext, TextWriter writer)
+            {
+                var pc = new WebPageContext();
+
+                const BindingFlags bindingFlags = BindingFlags.SetProperty
+                    | BindingFlags.Instance
+                    | BindingFlags.NonPublic;
+                typeof(WebPageContext).InvokeMember("HttpContext", bindingFlags,
+                    null, pc, new[] { viewContext.HttpContext });
+                typeof(WebPageContext).InvokeMember("ViewContext", bindingFlags,
+                    null, pc, new[] { viewContext });
+
+                var wb = WebPageBase.CreateInstanceFromVirtualPath(ViewPath);
+                wb.ExecutePageHierarchy(pc, writer);
+            }
+        }
+    }
