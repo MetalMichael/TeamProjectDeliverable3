d49b5e8d56f21f559d52cad6037667a9bd9a0d11
jleech92 <j.leech-11@student.lboro.ac.uk>
2014-03-19 19:42:20 +0000

    Added Module and Availability pages

--- Team Project/Models/TimetableSystemEntities.cs
+++ Team Project/Models/TimetableSystemEntities.cs
@@ -1,15 +1,17 @@
 ﻿using System.Data.Entity;
 
+
 namespace TimetableSystem.Models
 {
     public class TimetableSystemEntities : DbContext
     {
         public TimetableSystemEntities() : base("DefaultConnection") { }
-
+        
         public DbSet<Department> Departments { get; set; }
         public DbSet<Module> Modules { get; set; }
         public DbSet<Request> Requests { get; set; }
         public DbSet<Round> Rounds { get; set; }
         public DbSet<User> Users { get; set; }
     }
+
 }
