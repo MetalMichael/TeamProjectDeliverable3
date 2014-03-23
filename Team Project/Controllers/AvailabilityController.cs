7ab6c831e911b2438241fb13bdd8e80501e0a631
jleech92 <j.leech-11@student.lboro.ac.uk>
2014-03-23 19:54:41 +0000

    Revert "changes to availability page"
    This reverts commit 6b100af559cb4f6649dab52fd8382054b01bee6b.

--- Team Project/Controllers/AvailabilityController.cs
+++ Team Project/Controllers/AvailabilityController.cs
@@ -13,18 +13,6 @@ namespace TimetableSystem.Controllers
 
         public ActionResult Index()
         {
-            string[] park = { "East", "Central", "West" };
-            SelectList parkList = new SelectList(park);
-            ViewBag.Park = parkList;
-            string[] buildings = { "James France", "HazleGrave", "Schofield", "Brockington", "Edward Herbert Building", "Stewart Mason Building" };
-            SelectList buildingList = new SelectList(buildings);
-            ViewBag.Building = buildingList;
-            string[] room = { "J001", "J002", "N001", "N002", "N003", "U020" };
-            SelectList roomList = new SelectList(room);
-            ViewBag.Room = roomList;
-            int[] week = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-            SelectList weekList = new SelectList(week);
-            ViewBag.Week = weekList;
             return View();
         }
 
