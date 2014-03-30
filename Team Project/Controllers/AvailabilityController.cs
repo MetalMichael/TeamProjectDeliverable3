96f3cf75308df9493f7a3920a077b4d4e8ff4edb
jleech92 <j.leech-11@student.lboro.ac.uk>
2014-03-30 19:59:39 +0100

    ivjsdj

--- Team Project/Controllers/AvailabilityController.cs
+++ Team Project/Controllers/AvailabilityController.cs
@@ -19,32 +19,17 @@ namespace TimetableSystem.Controllers
             SelectList parkList = new SelectList(parkQry);
             ViewBag.Park = parkList;
 
-            //var buildList = new List<String>();
             var buildQry = from b in systemDB.Buildings
                            orderby b.BuildingName
                            select b.BuildingName;
-            //buildList.AddRange(buildQry.Distinct());
             SelectList buildList = new SelectList(buildQry);
             ViewBag.Building = buildList;
 
-            
             var roomQry = from r in systemDB.Rooms
                           orderby r.RoomCode
                           select r.RoomCode;
-            //roomList.AddRange(roomQry.Distinct());
             SelectList roomList = new SelectList(roomQry);
             ViewBag.Room = roomList;
-       
-            
-            /*string[] park = { "East", "Central", "West" };
-            SelectList parkList = new SelectList(park);
-            ViewBag.Park = parkList;
-            string[] buildings = { "James France", "HazleGrave", "Schofield", "Brockington", "Edward Herbert Building", "Stewart Mason Building" };
-            SelectList buildingList = new SelectList(buildings);
-            ViewBag.Building = buildingList;
-            string[] room = { "J001", "J002", "N001", "N002", "N003", "U020" };
-            SelectList roomList = new SelectList(room);
-            ViewBag.Room = roomList;*/
 
             int[] semester = { 1, 2 };
             SelectList semesterList = new SelectList(semester);
