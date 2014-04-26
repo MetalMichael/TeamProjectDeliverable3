371262eb126d07611ac098f1486b241129b90ada
MetalMichael <michaelfiford@gmail.com>
2014-04-26 17:05:55 +0100

    Convert index template to index.cshtml.
    Looks kinda of messy and things are broken. But it's progress...

--- Team Project/Controllers/HomeController.cs
+++ Team Project/Controllers/HomeController.cs
@@ -54,53 +54,35 @@ namespace TimetableSystem.Controllers
             return CreateForm(request);
         }
 
-        public static string DisplayObjectInfo(Object o)
-        {
-           StringBuilder sb = new StringBuilder();
-
-           // Include the type of the object
-           System.Type type = o.GetType();
-           sb.Append("Type: " + type.Name);
-
-           // Include information for each Field
-           sb.Append("\r\n\r\nFields:");
-           System.Reflection.FieldInfo[] fi = type.GetFields();
-           if (fi.Length > 0)
-            {
-              foreach (FieldInfo f in fi)
-              {
-                 sb.Append("\r\n " + f.ToString() + " = " +
-                           f.GetValue(o));
-              }
-           }
-           else
-              sb.Append("\r\n None");
-
-           // Include information for each Property
-           sb.Append("\r\n\r\nProperties:");
-           System.Reflection.PropertyInfo[] pi = type.GetProperties();
-           if (pi.Length > 0)
-           {
-              foreach (PropertyInfo p in pi)
-              {
-                 sb.Append("\r\n " + p.ToString() + " = " +
-                           p.GetValue(o, null));
-              }
-           }
-           else
-              sb.Append("\r\n None");
-
-           return sb.ToString();
-        }
-
-
         private ActionResult CreateForm(Request request)
         {
             ViewBag.Title = "Create new Request";
 
             ViewBag.Modules = new SelectList(db.Modules, "ModuleCode", "ModuleTitle");
+            ViewBag.ModuleCodes = new SelectList(db.Modules, "ModuleCode", "ModuleCode");
             ViewBag.Buildings = new SelectList(db.Buildings, "BuildingID", "BuildingName");
             ViewBag.Rooms = new SelectList(db.Rooms, "RoomID", "RoomCode");
+
+            var times = new List<String>();
+            for (var x = 9; x <= 17; x++)
+            {
+                string y = x.ToString();
+                if (y.Length == 1)
+                {
+                    y = "0" + y;
+                }
+                times.Add(y + ":00");
+            }
+            ViewBag.Times = new SelectList(times);
+
+            var lengths = new List<String>();
+            for (var x = 1; x < 10; x++)
+            {
+                lengths.Add(x.ToString());
+            }
+            ViewBag.Lengths = new SelectList(lengths);
+
+            ViewBag.Days = new[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
 
             ViewBag.WeekCheckboxes = "";
             for (int x = 1; x <= 13; x++)
