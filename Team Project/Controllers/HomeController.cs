37a2f4f64663bf00cf5672c0985a90e263650277
MetalMichael <metalmichael@hotmail.co.uk>
2014-05-13 20:49:48 +0100

    And again...

--- Team Project/Controllers/HomeController.cs
+++ Team Project/Controllers/HomeController.cs
@@ -190,7 +190,7 @@ namespace TimetableSystem.Controllers
             {
                 string y = x.ToString();
                 int t = x + 8;
-                times.Add(new SelectListItem { Text = "Period " + y + " : (" + t + ":00 - " + t + ":50)" , Value = x});
+                times.Add(new SelectListItem { Text = "Period " + y + " : (" + t + ":00 - " + t + ":50)" , Value = y });
             }
             ViewBag.Times = times;
 
